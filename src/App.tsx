59b29b64e8b116f152d67f475a5c1b10c1f9a082
Erismar Oliveira <erismarpro@hotmail.com>
2021-06-28 22:59:02 -0300

    Update class

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import './App.css';
 
-
 function App() {
-  return (
-    <div className="App-container">
-      <h1>Template Typescript</h1>
-    </div>
-  );
+    return (
+        <div className="container">
+            <h1>Template Typescript</h1>
+        </div>
+    );
 }
 
 export default App;
